903519948a2940f463f5ed403bd41a6fd41b7ad2
Konstantin <konstantinstepanyan99@gmail.com>
2021-03-17 19:02:36 +0300

    made grid-table responsive 99%

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -8,7 +8,8 @@ export default class Table extends Component {
         
         
     <section className="table">
-        <div className="table__item sources border-top_grey">
+        
+        <div data-id="sources" className="table__article table__item sources border-top_grey">
             <div className="content-container">
                 <svg className="table__circle" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
                     <circle cx="9.5" cy="9.65918" r="9.5" fill="#C4C4C4"/>
@@ -17,28 +18,28 @@ export default class Table extends Component {
             </div>
         </div>
                 
-        <div className="table__item clicks border-top_grey">
+        <div data-id="clicks" className="table__article table__item clicks border-top_grey">
             <span className="table__p">Клики</span>
         </div>
                 
-        <div className="table__item shows border-top_grey">
+        <div data-id="shows" className="table__article table__item shows border-top_grey">
             <span className="table__p">Показы</span>
         </div>
                 
-        <div className="table__item conversion-price border-top_grey">
+        <div data-id="conversion-price" className="table__article table__item conversion-price border-top_grey">
             <span className="table__p">Стоимость <br/> конверсии</span>
         </div>
                 
                 
-        <div className="table__item price border-top_grey">
+        <div data-id="price" className="table__article table__item price border-top_grey">
             <span className="table__p">Стоимость</span>
         </div>
                 
-        <div className="table__item conversion border-top_grey">
+        <div data-id="conversion" className="table__article table__item conversion border-top_grey">
             <span className="table__p">Конверсии</span>
         </div>
                 
-        <div className="table__item sales border-top_grey">
+        <div data-id="sales" className="table__article table__item sales border-top_grey">
             <span className="table__p">Продажи</span>
         </div>
         
@@ -78,6 +79,41 @@ export default class Table extends Component {
         
         <div className="table__item sales-count border-top_grey">
             <span className="table__p">10</span>
+        </div>
+        
+        
+        
+        <div data-id="sources_twin" className="table__article table__item table__item_twin sources_twin sources_twin_1 border-top_grey">
+            <div className="content-container">
+                <svg className="table__circle" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
+                    <circle cx="9.5" cy="9.65918" r="9.5" fill="#C4C4C4"/>
+                </svg>
+                <h4 className="table__title">Источники</h4>
+            </div>
+        </div>
+                
+        <div data-id="clicks_twin" className="table__article table__item table__item_twin clicks_twin clicks_twin_1 border-top_grey">
+            <span className="table__p">Клики</span>
+        </div>
+                
+        <div data-id="shows_twin" className="table__article table__item table__item_twin shows_twin shows_twin_1 border-top_grey">
+            <span className="table__p">Показы</span>
+        </div>
+        <div data-id="conversion-price_twin" className="table__article table__item table__item_twin conversion-price_twin  conversion-price_twin_1 border-top_grey">
+            <span className="table__p">Стоимость <br/> конверсии</span>
+        </div>
+                
+                
+        <div data-id="price_twin" className="table__article table__item table__item_twin price_twin price_twin_1 border-top_grey">
+            <span className="table__p">Стоимость</span>
+        </div>
+                
+        <div data-id="conversion_twin" className="table__article table__item table__item_twin conversion_twin conversion_twin_1 border-top_grey">
+            <span className="table__p">Конверсии</span>
+        </div>
+                
+        <div data-id="sales_twin" className="table__article table__item table__item_twin sales_twin_1 border-top_grey">
+            <span className="table__p">Продажи</span>
         </div>
         
         
