26b707df838c3fabce102ec7167c003cf9fd636b
Konstantin <konstantinstepanyan99@gmail.com>
2021-03-18 11:54:36 +0300

    added the ability to switch between menu items

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,18 +3,55 @@ import './css/menu.css';
 
 export default class Menu extends Component {
 
-  render() {
+   constructor(props) {
+    super(props);
+    this.state = {activeItem: 'item_sources'};
+  }
+
+    
+  componentDidMount() {
+      //this.setState is async func
+      this.setState({
+          menuItems: document.querySelectorAll('.menu__item')
+      }, () => {
+          this.toggleMenuItem(this.state.activeItem);
+          
+          for (let i of this.state.menuItems){
+              i.addEventListener('click', () => {
+                  this.toggleMenuItem(i.getAttribute('data-id'))
+              });
+          }
+      });
+      
+  }
+    
+    
+  toggleMenuItem(selectedItem){
+      
+      for (let i of this.state.menuItems){
+          i.classList.remove('item_active');
+          if(i.getAttribute('data-id') == selectedItem){
+              this.setState({
+                  activeItem: selectedItem
+              });
+              i.classList.add('item_active');
+          }
+      }
+  }
+
+    
+  render() {      
     return(
         
         
     <div className="menu-wrapper">
         <div className="menu">
-            <span className="menu__item">Сделки</span>
-            <span className="menu__item">Источники</span>
-            <span className="menu__item">Кампании</span>
-            <span className="menu__item">Группы</span>
-            <span className="menu__item">Ключевые слова</span>
-            <span className="menu__item">Объявления </span>
+            <span data-id="item_dealings" className="menu__item">Сделки</span>
+            <span data-id="item_sources" className="menu__item">Источники</span>
+            <span data-id="item_companies" className="menu__item">Компании</span>
+            <span data-id="item_groups" className="menu__item">Группы</span>
+            <span data-id="item_keywords" className="menu__item">Ключевые слова</span>
+            <span data-id="item_ads" className="menu__item">Объявления </span>
         </div>
     </div>
         
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/table.css';
+import arrowDown from './img/arrow.svg';
 
 export default class Table extends Component {
 
@@ -14,7 +15,11 @@ export default class Table extends Component {
                 <svg className="table__circle" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
                     <circle cx="9.5" cy="9.65918" r="9.5" fill="#C4C4C4"/>
                 </svg>
-                <h4 className="table__title">Источники</h4>
+                <h4 className="table__title">Источники
+                    <img className="table__img img_arrow" src={arrowDown}/>
+                </h4>
+        
+        
             </div>
         </div>
                 
