f558598c90de622f0ad23233b3265e7b99263bce
Konstantin <konstantinstepanyan99@gmail.com>
2021-03-19 21:54:42 +0300

    add 3rows to groups, keywords, adds

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -10,7 +10,8 @@ export default class Table extends Component {
     
       this.state = {
           tableRow1: '',
-          tableRow2: ''   
+          tableRow2: '',
+          tableRow3: ''
       }
         this.checkTableTitle();
   }
@@ -42,7 +43,8 @@ export default class Table extends Component {
                     tableRow2: (
                     <TableRow serviceTitle="GoogleAds" serviceTitleDesc="146-362-7099" clicksCount="900" 
                     showsCount="18000" conversionPriceCount="931 ₽" priceCount="186200" conversionCount="200"
-                    salesCount="40" circleColor="#f00" />)
+                    salesCount="40" circleColor="#f00" />),
+                    tableRow3: ''
                 })                
           break;
                 
@@ -58,7 +60,8 @@ export default class Table extends Component {
                 showsCount="8520" conversionPriceCount="1025 ₽" priceCount="102500" conversionCount="100"
                salesCount="10" circleColor="#00B355" />
                 ),
-                tableRow2: ''
+                tableRow2: '',
+                tableRow3: ''
             }
             );
 
@@ -71,13 +74,16 @@ export default class Table extends Component {
 //                salesCount="10" circleColor="#00B355"/>;
             this.setState({
                 tableRow1: (
-                    <TableRow serviceTitle="Группа1" serviceTitleDesc="123" clicksCount="852" 
+                    <TableRow serviceTitle="Группа 1" serviceTitleDesc="123" clicksCount="852" 
                 showsCount="8520" conversionPriceCount="1025 ₽" priceCount="102500" conversionCount="100"
                salesCount="10" circleColor="#00B355" />
                 ),
-                tableRow2: (<TableRow serviceTitle="Группа2" serviceTitleDesc="" clicksCount="" 
-                showsCount="" conversionPriceCount="" priceCount="" conversionCount=""
-               salesCount="" circleColor="red" />)
+                tableRow2: (<TableRow serviceTitle="Группа 2" serviceTitleDesc="23" clicksCount="14" 
+                showsCount="55" conversionPriceCount="3" priceCount="d" conversionCount="e"
+               salesCount="" circleColor="red" />),
+                tableRow3: (<TableRow serviceTitle="Группа 3" serviceTitleDesc="5" clicksCount="5" 
+                showsCount="123" conversionPriceCount="40" priceCount="5" conversionCount="2"
+               salesCount="1" circleColor="blue" />)
             });
 
           break;
@@ -93,9 +99,12 @@ export default class Table extends Component {
                 showsCount="8520" conversionPriceCount="1025 ₽" priceCount="102500" conversionCount="100"
                salesCount="10" circleColor="#00B355" />
                 ),
-                tableRow2: (<TableRow serviceTitle="Ключевое слово 1" serviceTitleDesc="" clicksCount="" 
+                tableRow2: (<TableRow serviceTitle="Ключевое слово 2" serviceTitleDesc="" clicksCount="" 
                 showsCount="" conversionPriceCount="" priceCount="" conversionCount=""
-               salesCount="" circleColor="red" />)
+               salesCount="" circleColor="red" />),
+                tableRow3: (<TableRow serviceTitle="Ключевое слово 3" serviceTitleDesc="123" clicksCount="456" 
+                showsCount="789" conversionPriceCount="22" priceCount="11" conversionCount="0"
+               salesCount="55" circleColor="blue" />)
             });
 
           break;
@@ -113,7 +122,10 @@ export default class Table extends Component {
                 ),
                 tableRow2: (<TableRow serviceTitle="Объявление 2" serviceTitleDesc="" clicksCount="" 
                 showsCount="" conversionPriceCount="" priceCount="" conversionCount=""
-               salesCount="" circleColor="red" />)
+               salesCount="" circleColor="red" />),
+                tableRow3: (<TableRow serviceTitle="Объявление 3" serviceTitleDesc="" clicksCount="" 
+                showsCount="" conversionPriceCount="" priceCount="" conversionCount=""
+               salesCount="" circleColor="blue" />)
             });
 
           break;
@@ -227,6 +239,43 @@ export default class Table extends Component {
         
         
         {this.state.tableRow2}
+        
+        
+        <div data-id="sources_twin" className="table__article table__item table__item_twin sources_twin sources_twin_2 border-top_grey">
+            <div className="content-container">
+                <svg className="table__circle" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
+                    <circle cx="9.5" cy="9.65918" r="9.5" fill="#C4C4C4"/>
+                </svg>
+                <h4 className="table__title">Источники</h4>
+            </div>
+        </div>
+                
+        <div data-id="clicks_twin" className="table__article table__item table__item_twin clicks_twin clicks_twin_2 border-top_grey">
+            <span className="table__p">Клики</span>
+        </div>
+                
+        <div data-id="shows_twin" className="table__article table__item table__item_twin shows_twin shows_twin_2 border-top_grey">
+            <span className="table__p">Показы</span>
+        </div>
+        <div data-id="conversion-price_twin" className="table__article table__item table__item_twin conversion-price_twin  conversion-price_twin_2 border-top_grey">
+            <span className="table__p">Стоимость <br/> конверсии</span>
+        </div>
+                
+                
+        <div data-id="price_twin" className="table__article table__item table__item_twin price_twin price_twin_2 border-top_grey">
+            <span className="table__p">Стоимость</span>
+        </div>
+                
+        <div data-id="conversion_twin" className="table__article table__item table__item_twin conversion_twin conversion_twin_2 border-top_grey">
+            <span className="table__p">Конверсии</span>
+        </div>
+                
+        <div data-id="sales_twin" className="table__article table__item table__item_twin sales_twin_2 border-top_grey">
+            <span className="table__p">Продажи</span>
+        </div>
+        
+        
+        {this.state.tableRow3}
         
         
         
